9660f1e0b69dd0bb5078e9dff14e1e23fda8325c
chequ <453177652@qq.com>
2022-06-21 01:38:24 +0800

    实时选房优化

--- pages/remaining/index.js
+++ pages/remaining/index.js
@@ -19,6 +19,143 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     floorHouseNum: 0, // 楼层房号最多的数量
     activeTab: 0, //楼栋切换
+    openDetailIdx: null, //房号详情展示
+    result: ['1', '3'], //已选过滤条件
+    conditionArr: [
+      {
+        title:'选择楼栋',
+        type:'checkbox',
+        checked: false,
+        key: 'floor',
+        value: [],
+        conditionData: {
+          list: [
+            {
+              label: '1栋',
+              name: '1'
+            },
+            {
+              label: '2栋',
+              name: '2'
+            }
+          ]
+        }
+      },
+      {
+        title:'选择户型',
+        type:'checkbox',
+        checked: false,
+        key: 'household',
+        value: [],
+        conditionData: {
+          list: [
+            {
+              label: 'A户·1号位',
+              name: '1'
+            },
+            {
+              label: 'B户·2号位',
+              name: '2'
+            }
+          ]
+        }
+      },
+      {
+        title:'选择建筑面积',
+        type:'checkbox',
+        checked: false,
+        key: 'area',
+        value: [],
+        conditionData: {
+          list: [
+            {
+              label: '138平',
+              name: '1'
+            },
+            {
+              label: '99平',
+              name: '2'
+            }
+          ]
+        }
+      },
+      {
+        title:'选择房屋属性',
+        type:'checkbox',
+        checked: false,
+        key: 'houseType',
+        value: [],
+        conditionData: {
+          list: [
+            {
+              label: '住宅',
+              name: '1'
+            },
+            {
+              label: '公寓',
+              name: '2'
+            }
+          ]
+        }
+      },
+      {
+        title:'选择朝向',
+        type:'checkbox',
+        checked: false,
+        key: 'towards',
+        value: [],
+        conditionData: {
+          list: [
+            {
+              label: '东北',
+              name: '1'
+            },
+            {
+              label: '南向',
+              name: '2'
+            }
+          ]
+        }
+      },
+      {
+        title:'输入均价范围',
+        type:'input',
+        checked: false,
+        key: 'avePrice',
+        value: [],
+        conditionData: {
+          list: [
+            {
+              label: '最低单价',
+              name: '1'
+            },
+            {
+              label: '最高单价',
+              name: '2'
+            }
+          ]
+        }
+      },
+      {
+        title:'输入总价范围',
+        type:'input',
+        checked: false,
+        key: 'totalPrice',
+        value: [],
+        conditionData: {
+          list: [
+            {
+              label: '最低总价',
+              name: '1'
+            },
+            {
+              label: '最高总价',
+              name: '2'
+            }
+          ]
+        }
+      }
+    ], // 过滤条件
   },
   onLoad(options) {
     let that = this
@@ -185,15 +322,17 @@ Page({
       floorHouseNum: that.mostHouseFloor(backTableData),
       listData:[
         {
-          buildNo: '1',
-          buildFloor:'5',
-          houseNo: '4',
+          buildNo: '100',
+          buildFloor:'500',
+          houseNo: '4000',
           houseArea: '125',
           houseNum: '三室一厅',
           houseType: '公寓',
           houseDirection: '西南',
           houseSunshineTime: '2小时',
-          housePrice:'2357776'
+          housePrice:'2357.77万',
+          renovated: false,
+          saled: false,
         },
         {
           buildNo: '1',
@@ -204,7 +343,9 @@ Page({
           houseType: '公寓',
           houseDirection: '西南',
           houseSunshineTime: '2小时',
-          housePrice:'2357776'
+          housePrice:'2357776',
+          renovated: true,
+          saled: true,
         },
         {
           buildNo: '1',
@@ -215,7 +356,9 @@ Page({
           houseType: '公寓',
           houseDirection: '西南',
           houseSunshineTime: '2小时',
-          housePrice:'2357776'
+          housePrice:'2357776',
+          renovated: true,
+          saled: false,
         },
         {
           buildNo: '1',
@@ -226,7 +369,9 @@ Page({
           houseType: '公寓',
           houseDirection: '西南',
           houseSunshineTime: '2小时',
-          housePrice:'2357776'
+          housePrice:'2357776',
+          renovated: true,
+          saled: false,
         },
         {
           buildNo: '1',
@@ -237,7 +382,9 @@ Page({
           houseType: '公寓',
           houseDirection: '西南',
           houseSunshineTime: '2小时',
-          housePrice:'2357776'
+          housePrice:'2357776',
+          renovated: true,
+          saled: false,
         },
         {
           buildNo: '1',
@@ -248,7 +395,9 @@ Page({
           houseType: '公寓',
           houseDirection: '西南',
           houseSunshineTime: '2小时',
-          housePrice:'2357776'
+          housePrice:'2357776',
+          renovated: true,
+          saled: false,
         },
         {
           buildNo: '1',
@@ -259,9 +408,28 @@ Page({
           houseType: '公寓',
           houseDirection: '西南',
           houseSunshineTime: '2小时',
-          housePrice:'2357776'
+          housePrice:'2357776',
+          renovated: true,
+          saled: false,
         }
       ]
+    })
+  },
+  // 收起过滤条件
+  toggle(event) {
+    const { index } = event.currentTarget.dataset;
+    const checkbox = this.selectComponent(`.checkboxes-${index}`);
+    checkbox.toggle();
+  },
+  noop() {},
+  onCheckChange(event) {
+    this.data.conditionArr.forEach((el,idx) => {
+      if (el.key === event.target.dataset.key) {
+        const _k1 = `conditionArr[${idx}].value`;
+        this.setData({
+          [_k1]: event.detail,
+        });
+      }
     })
   },
   // 找出最多房号的楼层
@@ -309,5 +477,40 @@ Page({
   },
   getHouseList() {
     this.closePopup()
-  }
+  },
+  // 列表房源详情显示
+  showDetail (e) {
+    this.setData({
+      openDetailIdx: e.target.dataset.idx
+    })
+  },
+  // 列表房源详情关闭
+  hideDetail (e) {
+    this.setData({
+      openDetailIdx:null
+    })
+  },
+  // 条件过滤变化
+  onConditionChange(event) {
+    this.setData({
+      result: event.detail
+    });
+    this.data.conditionArr.forEach((origin,idx) => {
+      let exg = event.detail.find(i => {
+        return i == idx
+      })
+      let _key = `conditionArr[${idx}].checked`;
+      let _val = `conditionArr[${idx}].value`;
+      if(exg){
+        this.setData({
+          [_key]: true,
+        });
+      }else{
+        this.setData({
+          [_key]: false,
+          [_val]: []
+        });
+      }
+    })
+  },
 })
